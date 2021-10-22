77a89717c740f1a7396c7f04cc025af98fb99622
FrancoCordoba <cordobafranco63@gmail.com>
2021-10-22 12:30:01 -0300

    Se termino el ejercicio

--- EjercicioN4/Assets/Scripts/BulletSript.cs
+++ EjercicioN4/Assets/Scripts/BulletSript.cs
@@ -6,15 +6,27 @@ public class BulletSript : MonoBehaviour
 {
     // Start is called before the first frame update
     public float bulletSpeed = 0.0f;
+    public float bulletTime  = 3f;
+    public float bulletDestroy = 3;
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         bulletMovement(Vector3.left);
+        bulletDestroy -= Time.deltaTime;
+        if (bulletDestroy < 0)
+        {
+            Destroy(gameObject);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+
+            transform.localScale = transform.localScale * 2;
+        }
     }
     private void bulletMovement (Vector3 direction)
     {
--- EjercicioN4/Assets/Scripts/GunScript.cs
+++ EjercicioN4/Assets/Scripts/GunScript.cs
@@ -6,23 +6,27 @@ public class GunScript : MonoBehaviour
 {
     // Start is called before the first frame update
    
-    public float bulletRatio = 0.0f;
-    public GameObject prefabBullet;
+    
+    public          GameObject  prefabBullet;
+    public float    bulletTime    = 3f;
+    public float    startShooting = 0f;
+    public float    shootingRatio = 0f;
     
     void Start()
-    {
+    {             
+      InvokeRepeating("shootBullet",startShooting,shootingRatio);    
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+
     }
 
     private void shootBullet() 
     {
-        Instantiate(prefabBullet,transform.position, prefabBullet.transform.rotation );
-    
+      Instantiate(prefabBullet,transform.position, prefabBullet.transform.rotation );  
     }
 }
